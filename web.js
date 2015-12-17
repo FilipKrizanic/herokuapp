c88751412b5c6c1216d76475b425dd7488294ee8
Filip Krizanic <filip@hotelbird.de>
2015-12-17 12:36:00 +0100

    asd

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ console.log("env:"+process.env.PATH+"\n\n");
 console.log("NEW ONE:"+__dirname+"\n");
 
 var server=net.createServer(function(socket){
-		socket.setTimeout(6000,function(msg){console.log("module.exports.sendToSihot TCP Socket timed out:"+msg);socket.end();});
+		socket.setTimeout(6000,function(msg){console.log("TCP Socket timed out:"+msg);socket.end();});
 		socket.on('error',function(err){console.log("ERROR!:"+err);});
 		socket.on('data',function(msg){socket.write("Data receaved! Your IP is:"+socket.remoteAddress);});
 		socket.on('close',function(){console.log("Socket closed!");socket.destroy();});
